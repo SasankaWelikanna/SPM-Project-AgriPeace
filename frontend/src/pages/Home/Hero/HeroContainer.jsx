51205d3acf3b44fe7e3dec529e68fc64685ac40f
HeshanChamika <it22194930@my.sliit.lk>
2024-08-06 15:00:02 +0530

    Update Swiper configuration for autoplay and speed

--- frontend/src/pages/Home/Hero/HeroContainer.jsx
+++ frontend/src/pages/Home/Hero/HeroContainer.jsx
@@ -1,7 +1,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-creative';
-import {EffectCreative} from 'swiper'
+import 'swiper/css/autoplay';
+import { EffectCreative, Autoplay } from 'swiper';
 import Hero from './Hero';
 import Hero2 from './Hero2';
 
@@ -9,33 +10,32 @@ const HeroContainer = () => {
   return (
     <section>
       <Swiper
-         grabCursor={true}
-         effect={"creative"}
-         creativeEffect={{
-            prev: {
-               shadow: true,
-               translate: ["-120%", 0, -500]
-            },
-            next: {
-               shadow: true,
-               translate: ["120%", 0, -500]
-            }
-         }}
-         modules={[EffectCreative]}
-         className='mySwiper5'
-         loop={true}
-         autoplay={
-            {
-               delay: 250,
-               disableOnInteraction: false
-            }
-         }
+        grabCursor={true}
+        effect={"creative"}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: ["-120%", 0, -500]
+          },
+          next: {
+            shadow: true,
+            translate: ["120%", 0, -500]
+          }
+        }}
+        modules={[EffectCreative, Autoplay]}
+        className='mySwiper5'
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false
+        }}
+        speed={1000}
       >
-         <SwiperSlide><Hero/></SwiperSlide>
-         <SwiperSlide><Hero2/></SwiperSlide>
+        <SwiperSlide><Hero /></SwiperSlide>
+        <SwiperSlide><Hero2 /></SwiperSlide>
       </Swiper>
     </section>
-  )
+  );
 }
 
-export default HeroContainer
+export default HeroContainer;
