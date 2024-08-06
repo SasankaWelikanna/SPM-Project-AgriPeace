8fd6682650794cbcae780e95bb1fe836b8ca86e2
HeshanChamika <it22194930@my.sliit.lk>
2024-08-06 19:45:26 +0530

    feat: Update button color in Services page and tailwind.config.js

--- frontend/src/pages/ContactUs/ContactUs.jsx
+++ frontend/src/pages/ContactUs/ContactUs.jsx
@@ -1,47 +1,49 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const ContactUs = () => {
   return (
     <>
-      <div class="flex justify-center items-center w-screen h-screen bg-white dark:bg-gray-900">
-        <div class="container mx-auto my-4 px-4 lg:px-20">
-          <div class="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl dark:bg-gray-700">
-            <div class="flex">
-              <h1 class="font-bold uppercase text-5xl dark:text-white">
+      <div className="flex justify-center items-center w-screen h-screen bg-white dark:bg-gray-900">
+        <div className="container mx-auto my-4 px-4 lg:px-20">
+          <div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl dark:bg-gray-700">
+            <div className="flex">
+              <h1 className="font-bold uppercase text-5xl dark:text-white">
                 Send us a <br /> message
               </h1>
             </div>
-            <div class="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
               <input
-                class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="text"
                 placeholder="First Name*"
               />
               <input
-                class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="text"
                 placeholder="Last Name*"
               />
               <input
-                class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="email"
                 placeholder="Email*"
               />
               <input
-                class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="number"
                 placeholder="Phone*"
               />
             </div>
-            <div class="my-4">
+            <div className="my-4">
               <textarea
                 placeholder="Message*"
-                class="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
               ></textarea>
             </div>
-            <div class="my-2 w-1/2 lg:w-1/4">
+            <div className="my-2 w-1/2 lg:w-1/4">
               <button
-                class="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
+                className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
                          focus:outline-none focus:shadow-outline"
               >
                 Send Message
@@ -49,56 +51,56 @@ const ContactUs = () => {
             </div>
           </div>
 
-          <div class="w-full lg:-mt-96 lg:w-2/6 px-8 py-12 ml-auto bg-blue-900 rounded-2xl">
-            <div class="flex flex-col text-white">
-              <h1 class="font-bold uppercase text-4xl my-4">
+          <div className="w-full lg:-mt-96 lg:w-2/6 px-8 py-12 ml-auto bg-blue-900 rounded-2xl">
+            <div className="flex flex-col text-white">
+              <h1 className="font-bold uppercase text-4xl my-4">
                 Drop in our office
               </h1>
-              <p class="text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                tincidunt arcu diam, eu feugiat felis fermentum id. Curabitur
-                vitae nibh viverra, auctor turpis sed, scelerisque ex.
+              <p className="text-gray-400">
+                Welcome to AgriPeace! We're dedicated to supporting sustainable
+                agriculture and empowering farmers with innovative solutions.
+                Visit our office or contact us for any inquiries.
               </p>
 
-              <div class="flex my-4 w-2/3 lg:w-1/2">
-                <div class="flex flex-col">
-                  <i class="fas fa-map-marker-alt pt-2 pr-2" />
+              <div className="flex my-4 w-2/3 lg:w-1/2">
+                <div className="flex flex-col">
+                  <i className="fas fa-map-marker-alt pt-2 pr-2" />
                 </div>
-                <div class="flex flex-col">
-                  <h2 class="text-2xl">Main Office</h2>
-                  <p class="text-gray-400">
-                    5555 Tailwind RD, Pleasant Grove, UT 73533
+                <div className="flex flex-col">
+                  <h2 className="text-2xl">Main Office</h2>
+                  <p className="text-gray-400">
+                    New Kandy Road, Malabe North, Western
                   </p>
                 </div>
               </div>
 
-              <div class="flex my-4 w-2/3 lg:w-1/2">
-                <div class="flex flex-col">
-                  <i class="fas fa-phone-alt pt-2 pr-2" />
+              <div className="flex my-4 w-2/3 lg:w-1/2">
+                <div className="flex flex-col">
+                  <i className="fas fa-phone-alt pt-2 pr-2" />
                 </div>
-                <div class="flex flex-col">
-                  <h2 class="text-2xl">Call Us</h2>
-                  <p class="text-gray-400">Tel: xxx-xxx-xxx</p>
-                  <p class="text-gray-400">Fax: xxx-xxx-xxx</p>
+                <div className="flex flex-col">
+                  <h2 className="text-2xl">Call Us</h2>
+                  <p className="text-gray-400">Tel: +94 117 544 801</p>
+                  <p className="text-gray-400">Fax: +94 11 241 3901</p>
                 </div>
               </div>
 
-              <div class="flex my-4 w-2/3 lg:w-1/2">
+              <div className="flex my-4 w-2/3 lg:w-1/2">
                 <a
-                  href="https://www.facebook.com/ENLIGHTENEERING/"
+                  href="#"
                   target="_blank"
                   rel="noreferrer"
-                  class="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1"
+                  className="rounded-full bg-white h-8 w-8 inline-flex items-center justify-center mx-1"
                 >
-                  <i class="fab fa-facebook-f text-blue-900" />
+                  <FaFacebookF className="text-blue-900" />
                 </a>
                 <a
-                  href="https://www.linkedin.com/company/enlighteneering-inc-"
+                  href="#"
                   target="_blank"
                   rel="noreferrer"
-                  class="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1"
+                  className="rounded-full bg-white h-8 w-8 inline-flex items-center justify-center mx-1"
                 >
-                  <i class="fab fa-linkedin-in text-blue-900" />
+                  <FaLinkedinIn className="text-blue-900" />
                 </a>
               </div>
             </div>
