ea7ac9993e158e9aeacc8880cee8f1da64e810c1
HeshanChamika <it22194930@my.sliit.lk>
2024-08-06 20:51:26 +0530

    chore: Update footer text in Footer component

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
             <div className="col-span-2">
               <div>
                 <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                  
+                  Get the latest news! 
                 </h2>
 
                 <p className="mt-4 text-gray-500 dark:text-gray-400">
