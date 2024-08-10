7a8e5efab8f77e271d77fa415539ec34b07846a8
HeshanChamika <it22194930@my.sliit.lk>
2024-08-10 13:52:34 +0530

    Update top margin in AboutUs and Services pages

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Scroll from "../../hooks/useScroll";
 
 const AboutUs = () => {
   return (
-    <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-900 mt-20">
+    <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-900 mt-10">
+      <Scroll/>
       <header className="text-center mb-12">
         <h1 className="text-4xl font-extrabold text-gray-900 dark:text-white mb-4">
           About Us
