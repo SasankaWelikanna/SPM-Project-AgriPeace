ef655fa8509108586643955ea8e16d71fcf5642b
Nadil Senruka <it22144430@my.sliit.lk>
2024-08-20 22:11:16 +0530

    start plant management front page
    start plant management front page

---
+++ backend/controllers/PlantManagement/PlantCtrl.js
@@ -0,0 +1,69 @@
+const Plant = require("../../models/PlantManagement/plant");
+
+const addPlant = async (req, res) => {
+  const { imageUrl, name, date, description } = req.body;
+  try {
+    const newPlant = await Plant.create({
+      imageUrl,
+      name,
+      date,
+      description,
+    });
+    res.json("New Plant Added");
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const getAllPlants = async (req, res) => {
+  try {
+    const plants = await FruitType.find();
+    res.json(plants);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const getOnePlant = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const plant = await Plant.findById(id);
+    res.status(200).json(plant);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const deletePlant = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Plant.findByIdAndDelete(id);
+    res.status(200).json({ message: "Fruit Type Deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const updatePlant = async (req, res) => {
+  const id = req.params.id;
+  const { imageUrl, name, date, description } = req.body;
+  try {
+    await Plant.findByIdAndUpdate(id, {
+      imageUrl,
+      name,
+      date,
+      description,
+    });
+    res.status(200).json({ message: "Fruit updated" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  addPlant,
+  getAllPlants,
+  getOnePlant,
+  deletePlant,
+  updatePlant,
+};
